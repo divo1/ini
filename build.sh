e18bdb8a2cab67b98763ffe00e2fd24c75debe0e
Michal Bojarski <fbojer@gmail.com>
2011-05-11 16:22:24 +0200

    modyfikacja

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
+echo Remove...
 rm -r *.o *.a *.exe
+echo Build...
+echo -----------------
 g++ -pipe -c CIni.cpp
 ar rcs libCIni.a CIni.o
 g++ -pipe main.cpp -L. -lCIni -o CIniTest
+echo -----------------
+echo End...
