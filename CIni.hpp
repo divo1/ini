e4fb23d87975913e142954fc08a149cf2a157439
divo <divo@divo.net.pl>
2011-10-05 17:22:07 +0300

    c["test"]["test2"] <- stworzy drzewo: [test] test2 Oczywiście test jest sekcją.
    Elementy puste nie są wyświetlane.
    
    Ustawianie wartości poprzez:
    c["test"]["test2"] = 2;
    c["test"]["test2"] = 2.0;
    c["test"]["test2"] = "2";

--- CIni.hpp
+++ CIni.hpp
@@ -1,97 +1,214 @@
 #include <iostream>
+
 #include <fstream>
+
 #include <string>
+
 #include <vector>
+
 #include <map>
+
 #include <cstdlib>
+
 #include <algorithm>
+
 #include <cstring>
+
 #include <sstream>
+
 #include <sys/stat.h>
+
 #include <sys/types.h>
 
+
+
 #ifndef CINIHPP
+
 #define CINIHPP 1
+
+
 
 #include "CIniException.hpp"
 
+
+
 using namespace std;
+
 class iniNode;
 
+
+
 class CIni {
+
 	private:
+
 		fstream file;
+
 		map<string, iniNode*> nodes;
+
 		string parent;
+
 		string lastName;
 
+
+
 		string filePath;
+
 		struct stat fileStats;
+
 	public:
+
 		friend ostream& operator << (ostream &out, CIni &c);
+
+		//friend ostream& operator << (ostream &out, CIni c);
+
 		CIni(string);
+
 		~CIni();
-		iniNode operator [] (string);
+
+		iniNode& operator [] (string);
+
 		operator string();
+
 		void addOrReplace(string);
+
 		void addOrReplaceFile(string);
+
 		void refresh();
+
 };
 
+
+
 class iniNode {
+
 	private:
+
 		iniNode* parent;
+
 		string name;
+
 		string value;
+
 		bool exist;
+
 		bool array;
+
 		void prepareString(string &s);
 
+
+
 		int arrayLength;
+
 	public:
+
 		friend ostream& operator << (ostream &out, iniNode &c);
-		friend ostream& operator << (ostream &out, iniNode c);
+
+		friend ostream& operator << (ostream &out, iniNode *c);
+
+		//friend ostream& operator << (ostream &out, iniNode c);
+
 		map<string, iniNode*> child;
+
 		iniNode(string, iniNode* parent = NULL);
+
 		iniNode(string, string, iniNode* parent = NULL);
 
+
+
 		void add(const char*);
+
 		void add(string&); // metoda do dodawania stringa do configa
+
 		void add(string, string);
+
+
 
 		void copy(iniNode*);
 
+
+
 		void setValue(string);
+
 		void setExist(bool);
+
 		int count();
 
-		iniNode operator [] (string);
-		iniNode operator [] (int);
+
+
+		iniNode& operator [] (string);
+
+		iniNode& operator [] (int);
+
+
+
+		iniNode& operator = (string);
+
+		iniNode& operator = (int);
+
+		iniNode& operator = (unsigned int);
+
+		iniNode& operator = (long int);
+
+		iniNode& operator = (double);
+
+		iniNode& operator = (float);
+
+
+
 		bool empty();
+
 		operator string();
+
 		string toString(int, string s = "");
+
 		string path();
+
 		string getName();
+
 		string* toArray();
 
+
+
 		// gettery
+
 		string get() throw(CIniException);
+
 		string get(string);
+
 		int get(int);
+
 		double get(double);
+
 		float get(float);
 
+
+
 		string getS() throw(CIniException);
+
 		string getS(string);
 
+
+
 		int getI() throw(CIniException);
+
 		int getI(int);
 
+
+
 		float getF() throw(CIniException);
+
 		float getF(float);
 
+
+
 		double getD() throw(CIniException);
+
 		double getD(double);
+
 };
 
+
+
 #endif
+
